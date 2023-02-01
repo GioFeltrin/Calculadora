8f6124cacf9fa601fe49f998fa44a076804ee431
GioFeltrin <giovanni.affornalli@gmail.com>
2023-01-31 22:17:48 -0300

    inserção de números e início de operações em JS

--- JS/index.js
+++ JS/index.js
@@ -4,22 +4,44 @@ const botao = document.querySelectorAll ("#botoes button");
 
 class Calculator {
     constructor(opAnteriorFunc, opInseridaFunc) {
-        this.opAnteriorFunc = opAnteriorFunc
-        this.opInseridaFunc = opInseridaFunc
-        this.opInserida = ""
+        this.opAnteriorFunc = opAnteriorFunc;
+        this.opInseridaFunc = opInseridaFunc;
+        this.opInserida = "";
 
+    }
+    addDigit(digit) {
+        if (digit === "." && this.opInseridaFunc.innerText.includes (".")) {
+            return;
+        }
+       
+        this.opInserida = digit;
+        this.updateScreen ();
+    }
+
+    processOperation (operation) {
+        let operationValue;
+        let Anterior = +this.opAnteriorFunc.innerText;
+        let Inserida = +this.opInseridaFunc.innerText;
+    }
+
+
+
+
+    updateScreen (){
+        this.opInseridaFunc.innerText += this.opInserida;
     }
 }
 
+const calc = new Calculator (opAnteriorFunc, opInseridaFunc)
 
 
 botao.forEach ((btn) => {
     btn.addEventListener('click', (e) => {
         const value = e.target.innerText;
         if (+value >= 0 || value === '.'){
-            console.log (value)
+            calc.addDigit(value);
         } else {
-            console.log ('op.' + value)
+            calc.processOperation (value)
         }
     })
 })
